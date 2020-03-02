5e9dd5c542253346c857922ca78ebfc50bdf3493
Neroi <975373787@qq.com>
2020-03-02 21:28:50 +0800

    modify urls.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,6 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# static-map = /static=/srv/django/static
 STATIC_ROOT = 'assets'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
@@ -143,27 +145,21 @@ MEDIA_URL = "/media/"  # 跟STATIC_URL类似，指定用户可以通过这个url
 #     )
 # }
 
-import environ
 
-# initialize env
+
 env = environ.Env(
-    # set casting, default value
+
     DEBUG=(bool, False)
 )
-# reading .env file
+
 environ.Env.read_env()
 ALLOWED_HOSTS = ['*']
-# False if not in os.environ
-DEBUG = env('DEBUG')
 
-# Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+DEBUG = env('DEBUG')
 SECRET_KEY = env('SECRET_KEY')
 
-# Parse database connection url strings like psql://user:pass@127.0.0.1:8458/db
 DATABASES = {
     'default': env.db(),
-    # read os.environ['SQLITE_URL']
-    # 'extra': env.db('SQLITE_URL', default='sqlite:////tmp/my-tmp-sqlite.db')
 }
 
 # CACHES = {
